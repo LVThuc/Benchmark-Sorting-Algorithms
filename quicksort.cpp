3a1961731df1b01953fd8752cb1d3f464edc7e7e
Thức Lê Văn <24521748@gm.uit.edu.vn>
2025-02-25 22:16:27 +0700

    Add files via upload

---
+++ quicksort.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int random(int a, int b)
+{
+    return a + rng() % (b - a + 1);
+}
+vector<int> arr;
+int partition(int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);  
+    return i + 1;
+}
+void quickSort(int low, int high) {
+  
+    if (low < high) {
+      
+
+        int pi = partition(low, high);
+        quickSort(low, pi - 1);
+        quickSort(pi + 1, high);
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+
+        int n;
+        cin >> n;
+        arr.resize(n);
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        quickSort(0, n - 1);
+}
+
